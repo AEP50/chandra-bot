f4189aca8d86f0ec858884e5a5478b15d69055c4
Sijia Wang <wangsijia0628@gmail.com>
2020-06-09 18:52:40 -0400

    travis

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+classifiers = [
+    "Development Status :: 1 - Planning",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+]
+
+with open("README.md") as f:
+    long_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+install_requires = [r.strip() for r in requirements]
+
+setup(
+    name="chandra_bot",
+    version="0.0.1",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adb40/chandra-bot",
+    license="Apache 2",
+    platforms="any",
+    packages=["chandra_bot"],
+    include_package_data=True,
+    install_requires=install_requires,
+)
